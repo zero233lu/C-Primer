de4916c623caf4fee26a6e647dea217b9f45b42c
zero233lu <37661828+zero233lu@users.noreply.github.com>
2018-03-23 23:21:21 +0800

    Add files via upload

---
+++ 3-24.cpp
@@ -0,0 +1,25 @@
+#include"stdafx.h"
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//	vector<int> v;
+//	int number;
+//	for (number; cin >> number; v.push_back(number));
+//	if (v.size() < 2)
+//	{
+//		cout << " please enter at least two integers";
+//		return -1;
+//	}
+//	for (auto it = v.cbegin(); it + 1 != v.cend(); ++it)
+//		cout << *it + *(it + 1) << " ";
+//	cout << endl;
+//
+//	for (auto lft = v.cbegin(), rht = v.cend() - 1; lft <= rht; ++lft, --rht)
+//		cout << *lft + *rht << " ";
+//	cout << endl;
+//
+//	return 0;
+//
+//}
---
+++ 3-25.cpp
@@ -0,0 +1,21 @@
+#include"stdafx.h"
+//#include<iostream>
+//#include<vector>
+//int main() {
+//	using namespace std;
+//	vector<int> s(11, 0);
+//	int grades;
+//	while (cin >> grades) {
+//		vector<int>::iterator it = s.begin();
+//		it += (grades / 10);
+//		++*it;
+//		cout << "++++++=" << endl;
+//		for (auto a : s) {
+//			cout << a << " ";
+//		}
+//	}
+//	cout << "aaaaaaaaaaaaaaaaaaaaa";
+//	for (auto a : s) {
+//		cout << a << " " << endl;
+//	}
+//}
